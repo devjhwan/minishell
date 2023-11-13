f0b6fa70d0f8e4be909c0ea25a9399d376e5d49c
Junghwan Lee <junghwle@cbr12s5.42barcelona.com>
2023-11-13 17:49:57 +0100

    save to home

--- src/parse/command_builder/translate_command_list.c
+++ src/parse/command_builder/translate_command_list.c
@@ -6,7 +6,7 @@
 /*   By: junghwle <junghwle@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 17:18:54 by junghwle          #+#    #+#             */
-/*   Updated: 2023/11/13 17:34:51 by junghwle         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:48:34 by junghwle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,47 @@
 #include "parser.h"
 #include "libft.h"
 
-t_cmnd	*append_new_command(t_cmnd **cmnd_list, t_io redir, char **args)
+static void	clear(t_io *redir, char **args)
+{
+	if (redir != NULL)
+	{
+
+	}
+}
+
+static t_cmnd	*append_new_command(t_cmnd **cmnd_list, \
+									t_io *redir, char **args)
+{
+	if (redir == NULL || args == NULL)
+}
+
+static t_io	*append_redir(t_io **redir_list, t_io *new_redir)
 {
 
 }
 
-t_io	*translate_redirection(t_redirection *redir)
+static t_io	*translate_redirection(t_command *command)
 {
-	t_io	*new_redir;
+	t_io			*redir_list;
+	t_io			*new_redir;
+	t_redirection	*redir;
 
-	if (redir == NULL)
-		return (NULL);
-	new_redir = (t_io *)malloc(sizeof(t_io));
-	if (new_redir == NULL)
-		return (free(redir->file), free(redir), NULL);
-	new_redir->type = (t_type)tredir->type;
-	new_redir->file = redir->file;
-	new_redir->next = NULL;
-	
-	free(redir);
+	redir = get_redirection(command);
+	while (redir != NULL)
+	{
+		new_redir = (t_io *)malloc(sizeof(t_io));
+		if (new_redir == NULL)
+			return (free(redir->file), free(redir), NULL);
+		new_redir->type = (t_type)tredir->type;
+		new_redir->file = redir->file;
+		new_redir->next = NULL;
+		if (redir_list == NULL)
+			redir_list = new_redir;
+		if (append_redir(&redir_list, new_redir) == NULL)
+			return (clear(redir_list, NULL), NULL);
+		free_redirection(redir);
+		redir = get_redirection(command);
+	}
 	return (new_redir);
 }
 
@@ -44,7 +66,11 @@ t_cmnd	*translate_command_list(t_list *command_list)
 	command = list_pop(command_list);
 	while (command != NULL)
 	{
-		append_new_command(&cmnd_list, )
+		if (append_new_command(&cmnd_list, \
+				translate_redirection(command),
+				get_command_arguments(command)) == NULL)
+			return (clear_command_list(cmnd_list), NULL);
 		free_command(command);
 	}
+	return (cmnd_list);
 }
